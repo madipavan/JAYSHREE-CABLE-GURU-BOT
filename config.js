9eee101a3ef20aa9b285d4280cfe9c9190297e2c
madipavan <125654570+madipavan@users.noreply.github.com>
2023-09-10 21:19:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['8878602068', 'me', true],
+  ['8878602068', 'Jayshree cable', true], 
   [''] 
 ] //Number of owners
 
